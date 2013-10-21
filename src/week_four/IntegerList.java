48afdf5a41dbbc04470bc2157b30e431d3d534bb
mbragg02 <mbragg02@cspc039.dcs.bbk.ac.uk>
2013-10-21 14:36:35 +0100

    integer list added

---
+++ src/week_four/IntegerList.java
@@ -0,0 +1,59 @@
+package week_four;
+
+import java.util.Scanner;
+
+
+public class IntegerList {
+	
+	private IntegerNode first;
+	
+	
+	public IntegerList() {
+		first = null;
+	}
+	
+	
+	public void add(int n) {
+
+		IntegerNode newNumber = new IntegerNode(n);
+		
+		if (first == null) {
+			first = newNumber;
+			return;
+		}
+		
+		IntegerNode aux = first;
+		while(aux.getNext() != null) {
+			aux = aux.getNext();
+		}
+		aux.setNext(newNumber);
+	}
+	
+	public void prettyPrint() {
+		IntegerNode current = first;
+		while(current != null) {
+			System.out.println(" " + current.getNumber());
+			current = current.getNext();
+		}
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		int input;
+		IntegerList list = new IntegerList();
+		Scanner in = new Scanner(System.in);
+
+		do {
+			
+			System.out.print("Please enter a number: ");
+			input = in.nextInt();
+			list.add(input);
+			
+		} while (input != 0);
+		in.close();
+		list.prettyPrint();
+	}
+
+}
