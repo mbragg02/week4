fa7a73d92d6e55325c45ad7c42c7d68af073cea2
Michael Bragg <mikeabragg@icloud.com>
2013-11-06 13:44:34 +0000

    pointer int stack test

---
+++ src/week_four/stacks/PointerIntStackTest.java
@@ -0,0 +1,40 @@
+package week_four.stacks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PointerIntStackTest {
+
+
+
+	@Test
+	public void testPush() {
+//		intStack firstStack = new pointerIntStack();
+//		firstStack.push(5);
+//		
+//		assertEquals(output, expected);
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPeek() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+}
